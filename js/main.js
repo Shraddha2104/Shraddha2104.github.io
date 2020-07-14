c1202eaa0ac16ef18826c892925a9322153ba8a0
Shraddha Shaligram <sshaligr@usc.edu>
2020-07-14 15:01:58 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -65,34 +65,34 @@
         }
     }
 
-    // Contact form validator
-    $(function () {
+//     // Contact form validator
+//     $(function () {
 
-        $('#contact-form').validator();
+//         $('#contact-form').validator();
 
-        $('#contact-form').on('submit', function (e) {
-            if (!e.isDefaultPrevented()) {
-                var url = "contact_form/contact_form.php";
+//         $('#contact-form').on('submit', function (e) {
+//             if (!e.isDefaultPrevented()) {
+//                 var url = "contact_form/contact_form.php";
 
-                $.ajax({
-                    type: "POST",
-                    url: url,
-                    data: $(this).serialize(),
-                    success: function (data) {
-                        var messageAlert = 'alert-' + data.type;
-                        var messageText = data.message;
+//                 $.ajax({
+//                     type: "POST",
+//                     url: url,
+//                     data: $(this).serialize(),
+//                     success: function (data) {
+//                         var messageAlert = 'alert-' + data.type;
+//                         var messageText = data.message;
 
-                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                        if (messageAlert && messageText) {
-                            $('#contact-form').find('.messages').html(alertBox);
-                            $('#contact-form')[0].reset();
-                        }
-                    }
-                });
-                return false;
-            }
-        });
-    });
+//                         var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+//                         if (messageAlert && messageText) {
+//                             $('#contact-form').find('.messages').html(alertBox);
+//                             $('#contact-form')[0].reset();
+//                         }
+//                     }
+//                 });
+//                 return false;
+//             }
+//         });
+//     });
     // /Contact form validator
 
     // Hide Mobile menu
